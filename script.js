c63b57646670277b69c9689e0a7d4f848a0a6e3e
AlexisFerreira <alexismatheus2013@gmail.com>
2024-07-31 23:39:27 -0300

    Desenvolvimento da função changeStyle usando classList

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 function changeStyle() {
-  //   construa aqui a sua lógica
-}
+  const divs = document.querySelectorAll(".block");
+  for(let i = 0; i < divs.length; i++){
+    const div = divs[i]
+    div.classList.toggle('circle')
+  }
+    const listItems = document.querySelectorAll("li")
+  
+  for(let i = 0; i < listItems.length; i++){
+    const li = listItems[i]
+      li.classList.toggle("itemCard") 
+  }
+  const spans = document.querySelectorAll("span")
+  const paragraphs = document.querySelectorAll("p")
+  
+  for(let i = 0; i < spans.length; i++){
+    const span = spans[i];
+    const p = paragraphs[i]
+
+    const online = span.classList.toggle("online");
+    
+    if(online){
+      span.classList.remove("offline")
+      p.innerText = "online"
+      
+    }else{
+      span.classList.add("offline")
+      p.innerText = "offiline"
+    }
+  }
+}     
+changeStyle()
